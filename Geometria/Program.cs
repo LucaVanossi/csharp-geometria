5d669f2c2aff7acc09241e9bfdb59cdebaac4570
LucaVanossi <vanossi.luca13@gmail.com>
2022-04-07 16:08:20 +0200

    Prove in corso

--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+namespace Geometria
+{
+    Rettangolo Rettangolo1 = new Rettangolo();  
+
+    /*class Program
+    {
+        static void Main (string[] args)
+        {
+            Rettangolo Rettangolo1 = new Rettangolo();
+           
+            //Rettangolo1.baseRettangolo = 15;
+            //Rettangolo1.altezzaRettangolo = 10;
+
+            Rettangolo1.altezzaRettangolo = Console.ReadLine();
+
+            Rettangolo1.calcolaArea();
+            Rettangolo1.calcolaPerimetro();
+
+            //Console.ReadKey();
+        }
+    }*/
+}
+
---
+++ Geometria/Rettangolo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    class Rettangolo
+    {
+        public int baseRettangolo;
+        public int altezzaRettangolo;
+
+        public void calcolaArea()
+        {
+            Console.WriteLine(baseRettangolo * altezzaRettangolo);
+        }
+
+        public void calcolaPerimetro()
+        {
+            Console.WriteLine((baseRettangolo + altezzaRettangolo) * 2);
+        }
+    }
+}
